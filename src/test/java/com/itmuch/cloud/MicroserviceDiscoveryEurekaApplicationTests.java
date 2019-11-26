c22d260899631fd56ee968900b6395da705e3702
USER <USER@MSI>
2019-11-26 11:44:18 +0800

    Eureka server

---
+++ src/test/java/com/itmuch/cloud/MicroserviceDiscoveryEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itmuch.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceDiscoveryEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
